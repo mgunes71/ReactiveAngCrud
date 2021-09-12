43045bf789229252993af5b6b5d36b4cd66dadf8
mgunes71 <mgnes9571@gmail.com>
2021-09-12 16:22:47 +0300

    angularCrud start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {AboutComponent} from "./components/about/about.component";
+import {UserComponent} from "./components/user/user.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:HomeComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'users', component:UserComponent},
+  {path: '**', component:HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavComponent} from './components/nav/nav.component';
+import {HomeComponent} from './components/home/home.component';
+
+import {AboutComponent} from './components/about/about.component';
+import {UserComponent} from "./components/user/user.component";
+import {ReactiveFormsModule} from "@angular/forms";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
